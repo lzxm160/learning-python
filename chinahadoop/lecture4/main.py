4f0a795ad961012ad8af1858be8b0ccb533d26de
lzxm160 <lzxm160@sina.com>
2017-02-25 09:09:42 +0800

    lecture

---
+++ chinahadoop/lecture4/main.py
@@ -0,0 +1,13 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def run_main():
+    html = urlopen("https://www.amazon.cn/gp/bestsellers/books/ref=br_bsl_smr/456-4063020-4086765?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=desktop-bestsellers-2&pf_rd_r=34EJ9KWD8JZF00TKW2V3&pf_rd_r=34EJ9KWD8JZF00TKW2V3&pf_rd_t=36701&pf_rd_p=777b26ab-395a-4110-95ea-35430219c976&pf_rd_p=777b26ab-395a-4110-95ea-35430219c976&pf_rd_i=desktop")
+    bs_obj = BeautifulSoup(html)
+
+    nav = bs_obj.find("span", {"class" : "nav-a-content"})
+    print nav.get_text()
+    
+if __name__ == '__main__':
+    run_main()
+
