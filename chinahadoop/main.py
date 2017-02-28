af01d5fb40417e22c33b2d0e0800a6fb8ea917d9
lzxm160 <lzxm160@sina.com>
2017-02-28 20:17:49 +0800

    scipy

--- chinahadoop/main.py
+++ chinahadoop/main.py
@@ -1,4 +1,7 @@
 import random
+import datetime
+import time
+
 print random.random()
 print random.uniform(1,9)
 print random.randrange(20)
@@ -11,3 +14,13 @@ print random.sample([1,2,3,4,5,6,7,8,9],5)
 weighted_choices=[('three',3),('two',2),('one',1)]
 population=[var for var,cnt in weighted_choices for i in range(cnt)]
 print random.choice(population)
+
+def next_5digit_int():
+	time.sleep(0.123)
+	current_time=datetime.datetime.now().time()
+	random_no=int(current_time.strftime('%S%f'))
+	return random_no/1000
+if __name__ == '__main__':
+	for x in range(0,10):
+		i=next_5digit_int()
+		print i
