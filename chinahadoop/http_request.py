bf675f43f2e8e62e189b070d19a61f45898dfbff
lzxm160 <lzxm160@sina.com>
2017-03-09 21:47:13 +0800

    test pdf

--- chinahadoop/http_request.py
+++ chinahadoop/http_request.py
@@ -115,6 +115,7 @@ def test_pdf():
 	start=time()
 	for num in range(0,10):
 		pdf_data="{\"src\":\"/root/Newshop_International/WEB-INF/quotation_pdf.html\",\"dst\":\"/root/go_fcgi/test"+str(num)+".pdf\"}"
+		print pdf_data
 		json_pdf_data = json.dumps(pdf_data)	
 		req = urllib2.Request("http://127.0.0.1:9888/pdf")
 		result = urllib2.urlopen(req,json_pdf_data)
