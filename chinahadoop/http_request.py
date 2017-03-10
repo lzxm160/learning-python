12e05f0115175f327c345d616e33d52563b8b863
lzxm160 <lzxm160@sina.com>
2017-03-10 17:43:48 +0800

    pdf

--- chinahadoop/http_request.py
+++ chinahadoop/http_request.py
@@ -146,8 +146,36 @@ def test_redis_scm_year():
 		print '\n'.join(result.readlines())
 	finish=time()
 	print (finish-start)*1000/50,"ms"
+def test_athenapdf():
+	start=time()
+	file_name="out.pdf"
+	for num in range(0,10):
+		req = urllib2.Request("http://127.0.0.1:8080/convert\?auth\=arachnys-weaver\&url\=http://news.baidu.com/")
+		u = urllib2.urlopen(req)
+		f = open(file_name, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+		    buffer = u.read(block_sz)
+		    if not buffer:
+		        break
+
+		    file_size_dl += len(buffer)
+		    f.write(buffer)
+		    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+		    status = status + chr(8)*(len(status)+1)
+		    print status,
+
+		f.close()
+	finish=time()
+	print (finish-start)*1000/10,"ms"
 if __name__ == '__main__':
-	test_pdf()
+	test_athenapdf()
+	# test_pdf()
 	# test_process_pdf()
 	# test_asiofcgi_pdf()
 	# test_process_pdf_online()
