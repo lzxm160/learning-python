5ad73f05846ec20e154446fac06601bc3dca62e4
lzxm160 <lzxm160@sina.com>
2017-03-20 21:17:57 +0800

    test

--- chinahadoop/learning.py
+++ chinahadoop/learning.py
@@ -21,6 +21,15 @@ def test_numpy2():
 	b=np.matrix('4 5 6;7 8 9;10 11 12')
 	print b
 	print a*b
+def test_numpy3():
+	import numpy.ma as ma
+	x=np.array([72,79,85,90,150,-135,120,-10,60,100])
+	mx=ma.mask_array(x,mask=[0,0,0,0,0,1,0,1,0,0])
+	mx2=ma.mask_array(x,mask=x<0)
+	print x.mean()
+	print mx.mean()
+	print mx2.mean()
 if __name__ == '__main__':
 	# test_numpy()
-	test_numpy2()
+	# test_numpy2()
+	test_numpy3()
