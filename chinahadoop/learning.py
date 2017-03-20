da8e81a29f998ec163a1a3857c01a19693c36043
lzxm160 <lzxm160@sina.com>
2017-03-20 20:39:32 +0800

    test

---
+++ chinahadoop/learning.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+def test_numpy():
+	x2d=np.array(((1,2,3),(4,5,6),(7,8,9)))
+	print x2d.shape
+	print x2d.dtype
+	print x2d.size
+	print x2d.itemsize
+	print x2d.ndim
+	print x2d.data
+if __name__ == '__main__':
+	test_numpy()
