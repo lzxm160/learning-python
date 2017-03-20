45fa4a8e557628aa6b2bb98032b7cb30908d6f37
lzxm160 <lzxm160@sina.com>
2017-03-20 20:48:58 +0800

    test

--- chinahadoop/learning.py
+++ chinahadoop/learning.py
@@ -1,12 +1,14 @@
 
 import numpy as np
 def test_numpy():
-	x2d=np.array(((1,2,3),(4,5,6),(7,8,9)))
-	print x2d.shape
-	print x2d.dtype
-	print x2d.size
-	print x2d.itemsize
-	print x2d.ndim
-	print x2d.data
+	# x2d=np.array(((1,2,3),(4,5,6),(7,8,9)))
+	# print x2d.shape
+	# print x2d.dtype
+	# print x2d.size
+	# print x2d.itemsize
+	# print x2d.ndim
+	# print x2d.data
+	x=np.array([1,2,3,4,5,6,7,8,9])
+	print x[3,7]
 if __name__ == '__main__':
 	test_numpy()
