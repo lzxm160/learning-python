5e3b44fcd4ef9158936ad1fbbf7ec136a32a2154
lzxm160 <lzxm160@sina.com>
2017-03-20 21:03:58 +0800

    test

--- chinahadoop/learning.py
+++ chinahadoop/learning.py
@@ -3,12 +3,16 @@ import numpy as np
 def test_numpy():
 	x2d=np.array(((1,2,3),(4,5,6),(7,8,9),(10,11,12),(13,14,15),(16,17,18)))
 	print x2d
+	print '-----------------'
 	x2d.ravel()
 	print x2d
+	print '-----------------'
 	x2d.resize(3,6)
 	print x2d
+	print '-----------------'
 	x2d.reshape(6,3)
 	print x2d
+	print '-----------------'
 	x2d.shape(9,2)
 	print x2d
 if __name__ == '__main__':
