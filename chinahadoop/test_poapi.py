25dab5dcb9bdde3ff42489d1facc23ac3545a00d
lzxm160 <lzxm160@sina.com>
2017-03-22 11:48:35 +0800

    po api

--- chinahadoop/test_poapi.py
+++ chinahadoop/test_poapi.py
@@ -6,60 +6,63 @@ import json
 
 
 data = {
-   "operation":"SubmitPO",
-   "data":{
-      "request_system":1,
-"request_time":"2017-02-16 08:00:00",
-      "purchase_order":{
-         "website":"France",
-         "company":"ReneSola France",
-         "bill_type":"Purchase Order",
-         "po_no":"PO-FR-20170216-001014",
-         "po_url":"/root/go_fcgi/go_fcgi",
-         "po_date":"2017-02-16 18:00:00",
-         "created_by":"po_no",
-"approved_by":"",
-         "status":1,
-"supplier":"Renesola Shanghai",
-         "requested_delivery_date":"2017-03-20 24:00:00",
-"trade_term":"EXW",
-         "payment_terms":"30 Days EOM The 15",
-         "ship_via":"Sea",
-         "export_country":"China",
-         "loading_port":"Shenzhen",
-         "import_country":"France",
-         "unloading_port":"Amsterdam",
-         "certificate":"",
-         "total_quantity":2400,
-         "total_amount":5690.47,
-         "currency":"EUR",
-         "comments":"comments1",
-         "note":"note1",
-         "detail":[{
-            "product_name":"Highbay",
-            "product_code":"RHB120X0302",
-            "item_no":"3518020400845",
-            "unit_price":3.64,
-            "quantity":1000,
-            "uom":"PCS",
-            "sub_total":3640.00,
-            "warranty":3,
-            "comments":"detail comments1",
-"note":"detail note1"
-},{
-            "product_name":"Flood Light",
-            "product_code":"RFL400AK01D06",
-            "item_no":"3518030601741",
-            "unit_price":6.89,
-            "quantity":200,
-            "uom":"PCS",
-            "sub_total":1378.00,
-            "warranty":3,
-            "comments":"detail comments2",
-"note":"detail note2"
-}]
-}
-   }
+    "request_system": 1,
+    "request_time": "2017-02-16 08:00:00",
+    "operation": "SubmitPO",
+    "data": {
+        "purchase_order": {
+            "website": "France",
+            "company": "ReneSola France",
+            "bill_type": "Purchase Order",
+            "po_no": "PO-FR-20170216-001014",
+            "po_url": "/opt/renesola/apollo/file/po/PO-FR-20170216-001014.pdf",
+            "po_date": "2017-02-16 18:00:00",
+            "created_by": "",
+            "approved_by": "",
+            "status": 1,
+            "supplier": "Renesola Shanghai",
+            "requested_delivery_date": "2017-03-20 24:00:00",
+            "trade_term": "EXW",
+            "payment_terms": "",
+            "ship_via": "Sea",
+            "export_country": "P.R.China",
+            "loading_port": "Shenzhen",
+            "import_country": "France",
+            "unloading_port": "Amsterdam",
+            "certificate": "",
+            "total_quantity": 2400,
+            "total_amount": 5690.47,
+            "currency": "EUR",
+            "comments": "",
+            "note": "",
+            "detail": [
+                {
+                    "product_name": "Highbay",
+                    "product_code": "RHB120X0302",
+                    "item_no": "3518020400845",
+                    "unit_price": 3.64,
+                    "quantity": 1000,
+                    "uom": "PCS",
+                    "sub_total": 3640,
+                    "warranty": 3,
+                    "comments": "",
+                    "note": ""
+                },
+                {
+                    "product_name": "Flood Light",
+                    "product_code": "RFL400AK01D06",
+                    "item_no": "3518030601741",
+                    "unit_price": 6.89,
+                    "quantity": 200,
+                    "uom": "PCS",
+                    "sub_total": 1378,
+                    "warranty": 3,
+                    "comments": "",
+                    "note": ""
+                }
+            ]
+        }
+    }
 }
 json_data = json.dumps(data)
 from time import clock
@@ -229,5 +232,5 @@ def test_gdn_data_api():
 	result = urllib2.urlopen(req, json_gdn_data)
 	print '\n'.join(result.readlines())
 if __name__ == '__main__':
-	# test_po_api()
-	test_gdn_data_api()
+	test_po_api()
+	# test_gdn_data_api()
