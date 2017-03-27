a5832415a08d74297e9c2a3974b97e7b6d6a1ab3
lzxm160 <lzxm160@sina.com>
2017-03-27 16:06:33 +0800

    po api

--- chinahadoop/test_poapi.py
+++ chinahadoop/test_poapi.py
@@ -13,7 +13,7 @@ data = {
         "purchase_order": {
             "company": "ReneSola France",
             "bill_type": "Purchase Order",
-            "po_no": "PO-FR-20170216-001242",
+            "po_no": "PO-FR-20170216-001243",
             "po_url": "/file/ci/CI-FR-20170226-000196.pdf",
             "po_date": "2017-02-16 18:00:00",
             "created_by": "Siyabonga gura",
@@ -85,8 +85,8 @@ gdn_data={
             {
                 "company": "ReneSola France",
                 "bill_type": "Goods Delivery Note",
-                "gdn_no": "GDN-FR-20170216-001242-009",
-                "po_no": "PO-FR-20170216-001242",
+                "gdn_no": "GDN-FR-20170216-001243-009",
+                "po_no": "PO-FR-20170216-001243",
                 "supplier": "Renesola Shanghai",
                 "buyer": "George Wang",
                 "trade_term": "CIF",
